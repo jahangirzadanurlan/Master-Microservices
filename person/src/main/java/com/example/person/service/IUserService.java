493d3091996348a80cba4cc22c04a4d63b74f5b6
jahangirzadanurlan <cahangirzadenurlan043@gmail.com>
2023-11-02 22:57:15 +0400

    second

--- person/src/main/java/com/example/person/service/IUserService.java
+++ person/src/main/java/com/example/person/service/IUserService.java
@@ -5,8 +5,15 @@ import com.example.person.model.entity.User;
 
 import java.util.List;
 
+/**
+ * @author Nurlan Jahangirzada
+ * @version 1.0
+ * @since 10.27.2023
+ */
 public interface IUserService {
     String saveUser(UserRequestDto userRequestDto);
     List<User> getAllUser();
 
 }
+
+
